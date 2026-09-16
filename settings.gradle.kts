3a44c2ddde5e613d58e5022cd246b9d8cde96920
MAN2ll <a.n.moiseev89@gmail.com>
2026-09-16 14:00:49 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,17 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -12,5 +19,10 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "SecureVault"
+
+rootProject.name = "MnemoKot"
+
+// Подключение модулей архитектуры
 include(":app")
+include(":core:crypto")
+include(":core:database")
